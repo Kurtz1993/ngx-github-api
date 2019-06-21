87a35c7c0d5d6695e2d96f44f83d6f243d1d5a81
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 10:55:00 -0500

    Make search component reactive

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,35 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { filter, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit, OnDestroy {
   @Output()
   search = new EventEmitter<string>();
 
+  userInput = new Subject<string>();
+  private sub: Subscription;
+
   searchText: string;
+
+  ngOnInit() {
+    this.sub = this.userInput
+      .pipe(
+        debounceTime(500),
+        filter(value => !!value)
+      )
+      .subscribe(value => {
+        this.search.emit(value);
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 
   searchUsername() {
     this.search.emit(this.searchText);
