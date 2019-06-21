640714afb5d2525acdecdc36d2b2143e291550da
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-06-21 10:56:59 -0500

    Implement repository card class

--- src/app/components/repository-card/repository-card.component.ts
+++ src/app/components/repository-card/repository-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Repository } from 'src/app/models/repository.model';
 
 @Component({
   selector: 'app-repository-card',
   templateUrl: './repository-card.component.html',
-  styleUrls: ['./repository-card.component.scss']
+  styleUrls: ['./repository-card.component.scss'],
 })
 export class RepositoryCardComponent implements OnInit {
+  @Input()
+  repository: Repository;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
